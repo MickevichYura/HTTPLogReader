8ad939b487334aee4b3b7258ceb4f18bfc6f50bc
hopheylalaley <mickevich.ja@gmail.com>
2015-03-06 22:47:51 +0300

    Обработка NumberFormatException

--- Log.java
+++ Log.java
@@ -56,6 +56,7 @@ public class Log {
 	}
 
 	public Log(String line) {
+		line = line.replaceAll("\r\n", " ");
 		char quotationMark = '\"';
 		int indexOfQuotationMark = line.indexOf(quotationMark);
 		int lastIndexOfQuotationMark = line.lastIndexOf(quotationMark);
@@ -66,8 +67,13 @@ public class Log {
 				lastIndexOfQuotationMark) + quotationMark;
 		this.replyCode = Integer.parseInt(line.substring(
 				lastIndexOfQuotationMark + 2, lastIndexOfSpace));
-		this.replyBytes = Integer
-				.parseInt(line.substring(lastIndexOfSpace + 1));
+
+		try {
+			this.replyBytes = Integer
+					.parseInt(line.substring(lastIndexOfSpace + 1));
+		} catch (NumberFormatException e) {
+			this.replyBytes = 0;
+		}
 
 		String timestampPattern = "[dd/MMM/yyyy:HH:mm:ss Z]";
 		this.timestamp = new SimpleDateFormat(timestampPattern, Locale.US)
